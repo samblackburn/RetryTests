8dff84c3c97a3f05434ce63c83fe2a7ef0a63d6e
Sam Blackburn <sam.blackburn@red-gate.com>
2025-10-17 16:37:09 +0100

    Exclamations work if backslash-escaped and then URL-encoded

--- Tests.cs
+++ Tests.cs
@@ -5,7 +5,7 @@ public class Tests
     [Test]
     public void FlakyTest([Values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] int tens,
                           [Values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)] int units,
-                          [Values("`%^&*()_+-=")] string foo)
+                          [Values("`!$%^&*()_+-=")] string foo)
     {
         if (new Random().Next(0, 50) > 0)
         {
--- run-tests.ps1
+++ run-tests.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-dotnet test --logger:"nunit;LogFileName=attempt1.xml"
+dotnet test --logger:"nunit;LogFileName=attempt1.xml" | Out-Null
 if ($LASTEXITCODE -eq 0) {
     "Tests passed first time"
     return;
@@ -13,7 +13,7 @@ $root.'test-run'.'test-suite'.'test-suite'.'test-suite'.'test-case' `
 | ForEach-Object {
     # Escape parentheses around test cases
     $testName = $_.fullname -replace '\(', '\(' -replace '\)', '\)'
-    $escapedTestName = [uri]::EscapeDataString($testName)
+    $escapedTestName = [uri]::EscapeDataString(($testName -replace '!', '\!'))
     $resultFile = $_.fullname -replace '[^a-zA-Z0-9]', '_'
     "Retrying test $testName with filter $escapedTestName"
     dotnet test --filter "FullyQualifiedName=$escapedTestName" --no-build --logger:"nunit;LogFileName=retry-$resultFile.xml"
