2e0739ead4f9fd0270ba40819b470efd677205bf
Sam Blackburn <sam.blackburn@red-gate.com>
2025-10-17 14:20:07 +0100

    A passing test and a failing test

---
+++ Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace RetryTests;
+
+public class Tests
+{
+    [Test]
+    public void PassingTest()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void FailingTest()
+    {
+        Assert.Fail();
+    }
+}
